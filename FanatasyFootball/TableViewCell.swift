2e2f9d9d3e5fa494cb16113ec34b0a84082cd79a
Devansh Sharma <devanshsharma@Devanshs-MacBook-Air.local>
2017-05-05 23:42:18 +0530

    tableviewadded

---
+++ FanatasyFootball/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  FanatasyFootball
+//
+//  Created by Devansh Sharma on 04/05/17.
+//  Copyright © 2017 Devansh Sharma. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
